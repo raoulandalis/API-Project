2b85e8beb0df75eceb0f99eedd83f400a43444b0
Raoul Andalis <raoulandalis1@yahoo.com>
2023-04-16 23:02:51 -0400

    added options

--- backend/db/migrations/20230414203435-create-spot.js
+++ backend/db/migrations/20230414203435-create-spot.js
@@ -54,10 +54,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   down: async (queryInterface, Sequelize) => {
     options.tableName = 'Spots';
-    return queryInterface.dropTable('Spots');
+    return queryInterface.dropTable(options);
   }
 };
