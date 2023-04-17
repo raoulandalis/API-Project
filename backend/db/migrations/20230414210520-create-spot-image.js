c91668e26c367ea1c92e8661b3a9e8710688acd8
Raoul Andalis <raoulandalis1@yahoo.com>
2023-04-16 23:13:31 -0400

    added options

--- backend/db/migrations/20230414210520-create-spot-image.js
+++ backend/db/migrations/20230414210520-create-spot-image.js
@@ -36,10 +36,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   down: async (queryInterface, Sequelize) => {
     options.tableName = "SpotImages";
-    return queryInterface.dropTable('SpotImages');
+    return queryInterface.dropTable(options);
   }
 };
